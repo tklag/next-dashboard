01cfe725209bc858b8b141d8fdcf50cefab6770e
tklag <t.kazuya111@gmail.com>
2024-04-07 08:49:02 +0930

    modify fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
-=======
-import { Inter, Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
->>>>>>> a406cc2262ced1172b1a1c4cfa5e6f24d9c8070a
 });
